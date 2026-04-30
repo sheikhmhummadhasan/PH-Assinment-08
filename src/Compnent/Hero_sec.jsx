5d5ec21bf8782cedf9923eb2d060c2a54db1d71d
sheikhmhummadhasan <hasan2001onlin@gmail.com>
2026-04-30 16:45:07 +0600

    hero section place

--- src/Compnent/Hero_sec.jsx
+++ src/Compnent/Hero_sec.jsx
@@ -1,15 +1,21 @@
+import { ArrowRight } from '@gravity-ui/icons';
 import Image from 'next/image';
 import React from 'react';
 
 const Hero_sec = () => {
     return (
         <div>
-            <div className="w-11/12 mx-auto">
-                <div className="">
-                    <h1 className="text-4xl">Never Stop Learning
+            <div className="w-11/12 mx-auto flex items-center justify-between">
+                <div className="mb-30">
+                    <h1 className="text-8xl">Never Stop Learning <br />
                         Life Never Stop Teaching</h1>
+                    <p className='text-[#545d70] my-2'>Every teaching and learning journey is unique Following
+                        We'll help guide your way.</p>
+                    <button className='flex items-center justify-center gap-2 py-2 px-5 rounded-full font-semibold text-white bg-[#5751e1]'> Start Free Trial <ArrowRight></ArrowRight></button>
                 </div>
-                <div className=""></div>
+                <div className="bg-[url('/hero-bg.png')] bg-no-repeat h-screen bg-center">
+                    <Image src={'/hero-student.png'} alt='hero student' height={500} width={500}></Image>
+                </div>
             </div>
         </div>
     );
